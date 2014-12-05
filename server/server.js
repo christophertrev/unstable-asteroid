54fc58eda1f1473e9f112e6a79be888ca5e629c6
noodle <noodle_292@yahoo.com>
2014-12-05 11:42:46 -0800

    (bug) Fix bug with setting up socket.io on server. Closes #32

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 var express = require('express');
-var http = require('http');
+var app = express();
+var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var mongoose = require('mongoose');
 var messageController = require('./messages/messageController');
 
 mongoose.connect('mongodb://MongoLab-d:tsWFfWiQkrxfZhKZbNOBPVGp3culnVTNs5G7nyd1cbE-@ds050077.mongolab.com:50077/MongoLab-d');
 
-var app = express();
 app.use(express.static(__dirname + '/../client') );
 app.use(express.static(__dirname + '/../client/styles') );
 
@@ -25,51 +25,4 @@ io.on('connection', function(socket) {
 });
 
 var port = process.env.PORT || 8000;
-app.listen(port);
-
-
-//*******
-//
-//create fake initial data until we're hooked up to the database
-//
-//*******
-
-// var messages = {
-//   _nextUniqueID: 0,
-//   _storage: [],
-
-//   root: undefined,
-
-//   addMessage: function(msg) {
-//     msg.id = messages._nextUniqueID++;
-//     msg.children = msg.children || [];
-//     messages._storage.push(msg);
-
-//     //set the added message as the root if no root
-//     if (!messages.root) {
-//       messages.root = msg;
-//       return;
-//     }
-
-//     //add the added message as a child to its parent
-//     if (msg.parentId !== undefined) {
-//       for (var i = 0; i < messages._storage.length; ++i) {
-//         var node = messages._storage[i];
-
-//         if (node.id === msg.parentId) {
-//           node.children.push(msg);
-//           break;
-//         }
-//       }
-//     }
-//   },
-
-//   getMessages: function() {
-//     return root;
-//   }
-// };
-
-// messages.addMessage({text: 'hi', parentId: null});
-// messages.addMessage({text: 'bye', parentId: 0});
-// messages.addMessage({text: 'what?', parentId: 0});
-// messages.addMessage({text: 'whatever', parentId: 1});
+http.listen(port);
