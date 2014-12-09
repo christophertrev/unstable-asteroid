5e9c1e7c550a351ae2b8253d1494a6566248876a
BethJohnson <BethJohnson3@gmail.com>
2014-12-08 17:06:15 -0800

    (docs) Adds docs for communication.js and display.js

--- client/js/communication.js
+++ client/js/communication.js
@@ -1,33 +1,27 @@
-
+//Socket class for sending/recieving messages from socket
+//Creates connection on object creation 
 var Socket = function(){
-  console.log('Attempting Connection');
   this.connection = io();
-  console.log(this.connection);
-
-  //what happens if there is not a connection available?!
 };
 
+//Sends message with new message event
 Socket.prototype.sendMessage = function(message){
-  console.log('sending message');
-  // console.log(message);
   this.connection.emit('new message', message);
 };
 
-Socket.prototype.onAllMessages = function(callback){
-  this.connection.on('all messages', function(messageReceived){
-    console.log('recieved all messages');
-    // console.log(messageReceived);
-    callback(messageReceived);
-  });
-};
-
+//Sends message with edit event
 Socket.prototype.sendEdit = function(message){
-  console.log('sending Edits for',message);
   this.connection.emit('edit message', message);
 };
 
+//Sends message with delete event
 Socket.prototype.sendDelete = function(message){
-  console.log('Sending delete for ',message);
   this.connection.emit('remove message leaf', message);
 };
 
+//Sets callback for when 'all messages' event is recieved
+Socket.prototype.onAllMessages = function(callback){
+  this.connection.on('all messages', function(messageReceived){
+    callback(messageReceived);
+  });
+};
